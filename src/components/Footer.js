ca618c14475c137bb4fefcdadf95776a41afff32
Prithiviraj69 <85940424+Prithiviraj69@users.noreply.github.com>
2021-12-24 09:25:20 +0530

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     const Login = useContext(Context);
     return (
         <footer>
-            {Login && <p>Copyright &copy; 2021 </p>}
-            {Login && <p>Email me :<a href="https://mail.google.com/mail/?view=cm&fs=1&to=sudhikumar8055@gmail.com"> SUDHI</a></p>}
+           
+            {Login && <p>Email me at:<a href="https://mail.google.com/mail/?view=cm&fs=1&to=prithvirajmudiliyar@gmail.com"> PRITHIVIRAJ ELUMALAI</a></p>}
             {Login && <Link to="/about">About</Link>}
             
         </footer>
